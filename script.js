a0bb8e316f496c88c48f7c4650c7612c151ddb23
Thais Cristina <cwbthais.l@gmail.com>
2022-04-15 20:12:35 -0300

    Correção no parâmetro da api

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ window.addEventListener('load', function() {
     $("#form").submit(function(e) {
         $('#cardNAsa').css('display', '')
         const dataLeitura = Object.fromEntries(new FormData(e.target).entries());
-
-        $.get('https://api.nasa.gov/planetary/apod?api_key=AhPeeOHbByxGgxb4Utxcm9HPRXLDdgc2igbihiMv',function(dataLeitura){
-            $('#titulo').html(dataLeitura.title);
-            $('#conteudo').html(dataLeitura.explanation);
-            $('#quemFez').html(dataLeitura.copyright);
-            if (dataLeitura.media_type == 'image') {
+        $.get(`https://api.nasa.gov/planetary/apod?api_key=AhPeeOHbByxGgxb4Utxcm9HPRXLDdgc2igbihiMv&date=${dataLeitura.dataParaPesquisar}`, function(response){
+            $('#titulo').html(response.title);
+            $('#conteudo').html(response.explanation);
+            $('#quemFez').html(response.copyright);
+            if (response.media_type == 'image') {
                 $("#imgOrVideo").append($('<img id="imgNasa" class="card-img-top">'));
-                $("#imgNasa").attr("src", dataLeitura.url);
+                $("#imgNasa").attr("src", response.url);
 
             }else{
                 var video = $('<video />', {
                     id: 'video',
-                    src: dataLeitura.media,
+                    src: response.media,
                     type: 'video/mp4',
                     controls: true
                 });
